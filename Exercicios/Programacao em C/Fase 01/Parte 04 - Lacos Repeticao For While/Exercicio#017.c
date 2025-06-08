18d802a58911cf886b6186c6e30670767cc367c8
Lorivan Lino Abreu <lorivan.lino@gmail.com>
2025-06-08 20:25:30 -0300

    Lorivan Lino de Abreu lorivan.lino@gmail.com

--- Exercicios/Programacao em C/Fase 01/Parte 04 - Lacos Repeticao For While/Exercicio#017.c
+++ Exercicios/Programacao em C/Fase 01/Parte 04 - Lacos Repeticao For While/Exercicio#017.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
-int main(){
-int numero
-while ( int i > 0)
-printf("Digite um numero: ");
-scanf("%d",&i);
+
+int main() {
+    int numero, soma = 0;
+    do {
+        printf("Digite um numero( 0 para sair): ");
+        scanf("%d", &numero);
+        soma = soma +numero;
+    } while (numero != 0);
+    printf("Soma dos numeros digitados : %d", soma);
+    return 0;
 }
